f3dc3492056482b97a391be28c1980aa0cb0f777
Rich-Harris <richard.a.harris@gmail.com>
2015-10-10 17:03:47 -0400

    use rollup-babel

--- gobblefile.js
+++ gobblefile.js
@@ -3,10 +3,8 @@ var path = require( 'path' );
 var resolve = require( 'resolve' );
 var Promise = require( 'es6-promise' ).Promise;
 
-var src = gobble( 'src' ).transform( 'babel' );
-
 module.exports = gobble([
-	src.transform( 'rollup', {
+	gobble( 'src' ).transform( 'rollup-babel', {
 		entry: 'index.js',
 		dest: 'magic-string.js',
 		format: 'umd',
@@ -16,7 +14,7 @@ module.exports = gobble([
 	}),
 
 	// version with deps (i.e. vlq) included
-	src.transform( 'rollup', {
+	gobble( 'src' ).transform( 'rollup-babel', {
 		entry: 'index.js',
 		dest: 'magic-string.deps.js',
 		format: 'umd',
