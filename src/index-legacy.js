1181b0a6617ac72fa2b45a6896ac0eaadd47b0a4
Pirate Praveen Arimbrathodiyil <praveen@debian.org>
2019-10-10 15:55:22 +0530

    export SourceMap in cjs/umd builds as well (#164)
    Currently SourceMap is exported only in ES module
    
    Originally reported as https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=942045

--- src/index-legacy.js
+++ src/index-legacy.js
@@ -1,7 +1,9 @@
 import MagicString from './MagicString.js';
 import Bundle from './Bundle.js';
+import SourceMap from './SourceMap.js';
 
 MagicString.Bundle = Bundle;
+MagicString.SourceMap = SourceMap;
 MagicString.default = MagicString; // work around TypeScript bug https://github.com/Rich-Harris/magic-string/pull/121
 
 export default MagicString;
